4403c814b5a5c74f05c3d559be450a4afd286a9c
aceleradora-la <info@aceleradora.la>
2025-11-25 17:46:52 -0300

    [REMOVE] Delete mrp_production_secondary_unit and mrp_stock_secondary_unit modules
    - Removing modules to start fresh
    
    - Will recreate with simpler approach

--- mrp_production_secondary_unit/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright 2025 Aceleradora
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
-from . import mrp_production
-from . import mrp_unbuild
-from . import stock_move
-
--- mrp_stock_secondary_unit/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright 2025 Aceleradora
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
-from . import mrp_production
-from . import stock_move
-
