93ccdfe132db64ea18c8d806b54939f18dc1ebe5
aceleradora-la <info@aceleradora.la>
2025-11-25 16:53:04 -0300

    [FIX] mrp_stock_secondary_unit: Add missing api import

--- mrp_stock_secondary_unit/models/stock_move.py
+++ mrp_stock_secondary_unit/models/stock_move.py
@@ -1,7 +1,7 @@
 # Copyright 2025 Aceleradora
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import api, models
 
 
 class StockMove(models.Model):
